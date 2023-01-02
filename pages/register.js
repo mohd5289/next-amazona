8cca6b1c3de5bfc53aa18a20913c8d8f3441a29a
Osuolale <mohd5289@yahoo.com>
2023-01-02 12:40:07 +0100

    SignUp Function and Page Completed

--- pages/register.js
+++ pages/register.js
@@ -7,6 +7,7 @@ import React,{ useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import Layout from '../components/Layout';
 import { toast } from 'react-toastify'
+import axios from 'axios'
 
 
 
@@ -22,11 +23,18 @@ function LoginScreen() {
     const{
         handleSubmit,
         register,
+        getValues,
         formState:{errors},
     }= useForm();
-const submitHandler= async({email,password})=>{
+const submitHandler= async({name,email,password})=>{
 console.log(email,password)
 try {
+    
+    await axios.post('/api/auth/signup',{
+      name,
+      email,
+      password,
+        });
     
     const result = await signIn('credentials',{
         redirect:false,
@@ -41,8 +49,15 @@ try {
 }
 }
     return (
-        <Layout title="Login"><form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
-            <h1 className='mb-4 text-xl'>Login</h1>
+        <Layout title="Register"><form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
+            <h1 className='mb-4 text-xl'>Create Account</h1>
+            <div className='mb-4'>
+           
+                <label htmlFor='name'>Name</label>
+                <input type="name"  {...register('name',{required:'Please enter name',})}
+                 className= "w-full" id="name" autoFocus></input>
+                   {errors.name && (<div className='text-red-500'>{errors.name.message}</div>)}
+            </div>
             <div className='mb-4'>
            
                 <label htmlFor='email'>Email</label>
@@ -66,7 +81,21 @@ try {
            {errors.password && (<div className='text-red-500'>{errors.password.message}
             </div>)} </div>
             <div className='mb-4'>
-                <button onClick={()=>router.push('login?redirect=/shipping')} className='primary-button'>Login</button>
+            
+                <label htmlFor='confirmPassword'>Confirm Password</label>
+
+                <input type="password" 
+                 {...register('confirmPassword',{required:'Please confirm password',
+                 validate:(value)=> value===getValues('password'),
+                minLength:{
+                    value:6,
+                    message:'confirm password is more than 6 chars',
+                },},)}
+                className= "w-full" id="confirmPassword" autoFocus></input>
+           {errors.confirmPassword && (<div className='text-red-500'>{errors.confirmPassword.message}
+            </div>)} {errors.confirmPassword && errors.confirmPassword.type==='validate'&&(<div className='text-red-500'>Password do not match </div>)}</div>
+            <div className='mb-4'>
+                <button onClick={()=>router.push('login?redirect=/shipping')} className='primary-button'>Register</button>
             </div>
             <div className='mb-4'>
                 Don&apos;t have an account? &nbsp;
